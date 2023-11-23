2d3cf686f00aefe865cc288e56bf5812b3891831
Muhammad Usama <imusamairshad@gmail.com>
2023-11-23 23:39:51 +0500

    splash_screen.dart is modified.

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:news_app/views/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +14,48 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const HomeScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final height = MediaQuery.sizeOf(context).height * 1;
+    // final width = MediaQuery.sizeOf(context).width * 1;
+    return Scaffold(
+      body: SizedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/splash_pic.jpg",
+              fit: BoxFit.cover,
+              height: height * .5,
+              // width: width * .9,
+            ),
+            SizedBox(height: height * 0.04),
+            Text(
+              "NEWS APP",
+              style: GoogleFonts.anton(
+                letterSpacing: .6,
+                color: Colors.grey.shade700,
+              ),
+            ),
+            SizedBox(height: height * 0.04),
+            const SpinKitChasingDots(
+              color: Colors.blue,
+              size: 40,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

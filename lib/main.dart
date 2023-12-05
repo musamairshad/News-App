6d3a5477c316e7fa6915268279894ced1ecc64bb
Muhammad Usama <imusamairshad@gmail.com>
2023-12-05 23:53:32 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/ui/splash/splash_screen.dart';
+import 'package:news_app/screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
